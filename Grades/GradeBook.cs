246c716eb23ba08d1e6ed6932ddab3ffdc94649a
Sherly Chan <sherlyc@gmail.com>
2017-08-08 22:37:59 +1200

    added constructor
    type ctor then tab twice to insert snippet of a constructor.

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,12 +8,14 @@ namespace Grades
 {
     class GradeBook
     {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
        public void AddGrade(float grade)
         {
             grades.Add(grade);   
         }
-        List<float> grades = new List<float>();
+        List<float> grades;
     }
-
-  
 }
